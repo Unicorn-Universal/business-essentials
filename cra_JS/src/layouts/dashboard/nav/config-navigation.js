c284c82eeec57410dc8935e9f99fba2be5ebda39
Persius A <44207168+persiusW@users.noreply.github.com>
2023-05-20 20:46:34 +0000

    Added Team page
    Added the team page and added the button to add team members.

--- cra_JS/src/layouts/dashboard/nav/config-navigation.js
+++ cra_JS/src/layouts/dashboard/nav/config-navigation.js
@@ -67,6 +67,17 @@ const navConfig = [
       ]
       },
 
+      {
+        items:[
+          { title: 'Team', 
+          path: PATH_DASHBOARD.user.cards,
+          icon: ICONS.user
+         },
+          
+        ]
+      },
+      
+
   // {
   //   title: 'user',
   //   path: PATH_DASHBOARD.user.root,
@@ -102,19 +113,19 @@ const navConfig = [
     subheader: 'management',
     items: [
       // USER
-      {
-        title: 'user',
-        path: PATH_DASHBOARD.user.root,
-        icon: ICONS.user,
-        children: [
-          { title: 'profile', path: PATH_DASHBOARD.user.profile },
-          { title: 'cards', path: PATH_DASHBOARD.user.cards },
-          { title: 'list', path: PATH_DASHBOARD.user.list },
-          { title: 'create', path: PATH_DASHBOARD.user.new },
-          { title: 'edit', path: PATH_DASHBOARD.user.demoEdit },
-          { title: 'account', path: PATH_DASHBOARD.user.account },
-        ],
-      },
+      // {
+      //   title: 'user',
+      //   path: PATH_DASHBOARD.user.root,
+      //   icon: ICONS.user,
+      //   children: [
+      //     { title: 'profile', path: PATH_DASHBOARD.user.profile },
+      //     { title: 'cards', path: PATH_DASHBOARD.user.cards },
+      //     { title: 'list', path: PATH_DASHBOARD.user.list },
+      //     { title: 'create', path: PATH_DASHBOARD.user.new },
+      //     { title: 'edit', path: PATH_DASHBOARD.user.demoEdit },
+      //     { title: 'account', path: PATH_DASHBOARD.user.account },
+      //   ],
+      // },
 
       // E-COMMERCE
       {
