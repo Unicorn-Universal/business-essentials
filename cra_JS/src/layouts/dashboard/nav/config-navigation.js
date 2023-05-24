a0b9006c8d0fe3617ca9d543e6f5018c35d4ea7b
Persius A <44207168+persiusW@users.noreply.github.com>
2023-05-24 03:44:59 +0000

    Cleaning up Code and front end

--- cra_JS/src/layouts/dashboard/nav/config-navigation.js
+++ cra_JS/src/layouts/dashboard/nav/config-navigation.js
@@ -92,24 +92,6 @@ const navConfig = [
         ]
         },
       
-
-  // {
-  //   title: 'user',
-  //   path: PATH_DASHBOARD.user.root,
-  //   icon: ICONS.user,
-  //   children: [
-  //     { title: 'profile', path: PATH_DASHBOARD.user.profile },
-  //     { title: 'cards', path: PATH_DASHBOARD.user.cards },
-  //     { title: 'list', path: PATH_DASHBOARD.user.list },
-  //     { title: 'create', path: PATH_DASHBOARD.user.new },
-  //     { title: 'edit', path: PATH_DASHBOARD.user.demoEdit },
-  //     { title: 'account', path: PATH_DASHBOARD.user.account },
-  //   ],
-  // },
-
-
-
-
   // {
   //   subheader: 'general',
   //   items: [
