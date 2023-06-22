2d66f7cb9967928da89c8b411be1a45c81c827af
Persius A <44207168+persiusW@users.noreply.github.com>
2023-06-22 18:28:13 +0000

    Updates

---
+++ static/js/3705.193f30ae.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_minimal_minimal_kit_react=self.webpackChunk_minimal_minimal_kit_react||[]).push([[3705],{33705:function(i,a,e){e.r(a);var m=e(8556);a.default=m.g}}]);
